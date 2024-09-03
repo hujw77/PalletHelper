5fa8d849b66441bd00997a068ee5f6e356d478c4
echo <hujw77@gmail.com>
2024-09-03 16:14:47 +0800

    pallet helper

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,9 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {PalletHelper} from "../scr/PalletHelper.sol";
 
 contract Deploy is Script {
+
+    address immutable CREATE2_ADDR = 0x4e59b44847b379578588920cA78FbF26c0B4956C;
+
     modifier broadcast() {
         vm.startBroadcast();
         _;
@@ -12,7 +15,14 @@ contract Deploy is Script {
     }
 
     function run() public broadcast {
-        Counter counter = new Counter();
-        counter.increment();
+        bytes memory code = type(PalletHelper).creationCode;
+		_deploy2(bytes32(0), code);
+	}
+
+    function _deploy2(bytes32 salt, bytes memory initCode) internal returns (address) {
+        if (CREATE2_ADDR.code.length == 0) revert CREATE2FactoryNotDeployed();
+        bytes memory data = bytes.concat(salt, initCode);
+        (, bytes memory addr) = CREATE2_ADDR.call(data);
+        return address(uint160(bytes20(addr)));
     }
 }
---
+++ src/PalletHelper.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@darwinia/contracts-utils/contracts/Bytes.sol";
+
+interface StateStorage {
+    function state_storage(bytes memory storageKey) external returns (bytes memory);
+}
+
+contract PalletHelper {
+    StateStorage constant STATE_STORAGE = StateStorage(0x0000000000000000000000000000000000000400);
+
+    bytes constant COLLATOR_COUNT_KEY = hex"03a4971484692cd58fa781fd333a29702bf10f943ea01b83e17db9b4b2ab031a";
+    bytes32 constant MIGRATION_START_TIME_KEY = 0x03a4971484692cd58fa781fd333a29706500b8b0f5e2d08ad46a290ff113be06;
+
+    function getContractCollators() public returns (uint256) {
+        bytes memory count_value = STATE_STORAGE.state_storage(COLLATOR_COUNT_KEY);
+        uint32 count = decodeUint32(count_value);
+
+        bytes memory migration_start_time_value =
+            STATE_STORAGE.state_storage(abi.encodePacked(MIGRATION_START_TIME_KEY));
+        uint128 migration_start_time = decodeUint128(migration_start_time_value);
+
+        uint256 diff = (block.timestamp - uint256(migration_start_time) / 1000) * 100 / 60 days;
+        return (diff >= 100) ? count : diff * count / 100;
+    }
+
+    function decodeUint32(bytes memory data) internal pure returns (uint32) {
+        require(data.length == 4, "Bad data");
+        bytes memory reversed = Bytes.reverse(data);
+        return uint32(Bytes.toBytes4(reversed, 0));
+    }
+
+    function decodeUint128(bytes memory data) internal pure returns (uint128) {
+        if (data.length == 0) return 0;
+        require(data.length == 16, "Bad data");
+        bytes memory reversed = Bytes.reverse(data);
+        return uint128(Bytes.toBytes16(reversed, 0));
+    }
+}
